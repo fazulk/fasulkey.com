0f755f53dccb28baf003d559a50c5839913b6b84
Jeff <fazulk@gmail.com>
2019-02-09 15:15:51 -0800

    Initial commit

---
+++ app.mjs
@@ -0,0 +1,25 @@
+import express from'express';
+import path from'path'
+import cookieParser from'cookie-parser'
+import logger from'morgan'
+
+import indexRouter from'./routes/index'
+import usersRouter from'./routes/users'
+
+const FILENAME = typeof __filename !== 'undefined' ? __filename : (/^ +at (?:file:\/*(?=\/)|)(.*?):\d+:\d+$/m.exec(Error().stack) || '')[1];
+const DIRNAME = typeof __dirname !== 'undefined' ? __dirname : FILENAME.replace(/[\/\\].*?$/, '');
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(DIRNAME, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+const CONNECTION = 'mongodb+srv://imabot69:crosby1027@topaz-vacfu.mongodb.net/test?retryWrites=true'
+
+export default app
