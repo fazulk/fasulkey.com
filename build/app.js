a48f3e1cd97b1a3d9018d6d2e12255d5c9527cdf
Jeff <fazulk@gmail.com>
2019-02-12 07:08:51 -0800

    fixed

--- build/app.js
+++ build/app.js
@@ -1,52 +1,48 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-require("@babel/polyfill");
+require('@babel/polyfill');
 
-var _dotenv = require("dotenv");
+var _dotenv = require('dotenv');
 
 var _dotenv2 = _interopRequireDefault(_dotenv);
 
-var _regeneratorRuntime = require("regenerator-runtime");
-
-var _regeneratorRuntime2 = _interopRequireDefault(_regeneratorRuntime);
-
-var _express = require("express");
+var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
-var _expressGraphql = require("express-graphql");
+var _expressGraphql = require('express-graphql');
 
 var _expressGraphql2 = _interopRequireDefault(_expressGraphql);
 
-var _mongoose = require("mongoose");
+var _mongoose = require('mongoose');
 
 var _mongoose2 = _interopRequireDefault(_mongoose);
 
-var _bodyParser = require("body-parser");
+var _bodyParser = require('body-parser');
 
 var _bodyParser2 = _interopRequireDefault(_bodyParser);
 
-var _cors = require("cors");
+var _cors = require('cors');
 
 var _cors2 = _interopRequireDefault(_cors);
 
-var _cookieParser = require("cookie-parser");
+var _cookieParser = require('cookie-parser');
 
 var _cookieParser2 = _interopRequireDefault(_cookieParser);
 
-var _morgan = require("morgan");
+var _morgan = require('morgan');
 
 var _morgan2 = _interopRequireDefault(_morgan);
 
-var _path = require("path");
+var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
 
-var _graphql = require("./graphql");
+var _graphql = require('./graphql');
 
 var _graphql2 = _interopRequireDefault(_graphql);
 
@@ -57,11 +53,8 @@ _dotenv2.default.config();
 
 var MONGO_CONNECTION = process.env.DB_CONNECTION;
 
-var FILENAME = typeof __filename !== "undefined" ? __filename : (/^ +at (?:file:\/*(?=\/)|)(.*?):\d+:\d+$/m.exec(Error().stack) || "")[1];
-var DIRNAME = typeof __dirname !== "undefined" ? __dirname : FILENAME.replace(/[\/\\].*?$/, "");
-
 var app = (0, _express2.default)();
-var URL = "http://localhost";
+var URL = 'http://localhost';
 var PORT = 3001;
 
 // Connect to MongoDB with Mongoose.
@@ -69,25 +62,25 @@ _mongoose2.default.connect(MONGO_CONNECTION, {
     useCreateIndex: true,
     useNewUrlParser: true
 }).then(function () {
-    return console.log("MongoDB connected");
+    return console.log('MongoDB connected');
 }).catch(function (err) {
     return console.log(err);
 });
 
-app.use((0, _morgan2.default)("dev"));
+app.use((0, _morgan2.default)('dev'));
 app.use(_express2.default.json());
 app.use(_express2.default.urlencoded({ extended: false }));
 app.use((0, _cookieParser2.default)());
-app.use(_express2.default.static(_path2.default.join(DIRNAME, "public")));
+app.use(_express2.default.static(_path2.default.join(__dirname, '../public')));
 app.use((0, _cors2.default)());
 
-app.use("/graphql", (0, _cors2.default)(), _bodyParser2.default.json(), (0, _expressGraphql2.default)({
+app.use('/graphql', (0, _cors2.default)(), _bodyParser2.default.json(), (0, _expressGraphql2.default)({
     schema: _graphql2.default,
     graphiql: true
 }));
 
 app.listen(PORT, function () {
-    console.log("Listening on " + URL + ":" + PORT);
+    console.log('Listening on ' + URL + ':' + PORT);
 });
 
 exports.default = app;
