8c28570d9478943bb035e3130bcb02ec0e6036a1
Jeff <fazulk@gmail.com>
2019-02-11 23:39:43 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import "@babel/polyfill";
 import dotenv from 'dotenv'
 dotenv.config()
-import regeneratorRuntime from "regenerator-runtime";
 import express from 'express'
 import expressGraphQL from 'express-graphql'
 import mongoose from 'mongoose'
@@ -14,15 +13,7 @@ import schema from './graphql'
 
 const MONGO_CONNECTION = process.env.DB_CONNECTION
 
-const FILENAME =
-    typeof __filename !== `undefined`
-        ? __filename
-        : (/^ +at (?:file:\/*(?=\/)|)(.*?):\d+:\d+$/m.exec(Error().stack) ||
-              ``)[1]
-const DIRNAME =
-    typeof __dirname !== `undefined`
-        ? __dirname
-        : FILENAME.replace(/[\/\\].*?$/, ``)
+
 
 const app = express()
 const URL = `http://localhost`
@@ -41,7 +32,7 @@ app.use(logger(`dev`))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(DIRNAME, `public`)))
+app.use(express.static(path.join(__dirname, `public`)))
 app.use(cors())
 
 app.use(
