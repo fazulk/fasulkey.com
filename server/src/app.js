1f6ded032320c2c5935fc432a16638993adb820b
fazulk <Ruskod0g!>
2019-03-08 23:31:26 -0800

    fi

--- server/src/app.js
+++ server/src/app.js
@@ -32,8 +32,15 @@ app.use(logger(`dev`))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, `../public`)))
 app.use(cors())
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'public')));
+    //
+    app.get('*', (req, res) => {
+        res.sendfile(path.join(__dirname = 'public/index.html'));
+    })
+}
 
 app.use(
     `/graphql`,
