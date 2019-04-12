687a9217c6b5ad791c2b88d593d33c3cfbd89a30
fazulk <Ruskod0g!>
2019-04-12 00:33:17 -0700

    f

--- vue_ui/src/router/index.js
+++ vue_ui/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../views/home.vue'
-import Resume from '../views/resume.vue'
+import Home from '@/views/home'
+import Resume from '@/views/resume'
 
 Vue.use(Router)
 
