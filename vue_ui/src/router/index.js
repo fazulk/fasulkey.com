dcf32bb4936cf6bd3d497f04fba50ccbfe3cd9d5
fazulk <Ruskod0g!>
2019-04-12 00:28:45 -0700

    fix

--- vue_ui/src/router/index.js
+++ vue_ui/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../views/home'
-import Resume from '../views/resume'
+import Home from '../views/home.vue'
+import Resume from '../views/resume.vue'
 
 Vue.use(Router)
 
