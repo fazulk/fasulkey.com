786145c0c4d9fd2010eb5529c875847039ccc1f0
fazulk <fazulk@gmail.com>
2019-08-12 16:45:31 -0700

    clean slate

--- server/src/routes/api/test/index.js
+++
@@ -1,7 +0,0 @@
-const router = require(`express`).Router()
-
-router.get(`/test`, (req, res) => {
-    res.sendStatus(200)
-})
-
-module.exports = router
--- server/src/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require(`express`).Router()
-const subRoutes = app_require(`routes/require_all_routes`)(__dirname)
-
-router.use(`/`, subRoutes)
-
-module.exports = router
