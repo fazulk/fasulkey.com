2bed5620730cd6d56e0928582a11244516e86e63
Jeff Fasulkey <fazulk@gmail.com>
2020-12-28 23:45:08 -0800

    feat(server): add set function for fire store

--- server/src/data/fasulkey/resume.js
+++ server/src/data/fasulkey/resume.js
@@ -23,7 +23,23 @@ export default function({ fasulkeyDb }) {
 
         return await db
             .collection(`resume`)
-            .doc()
-            .set()
+            .doc(`general`)
+            .set({
+                type: `non-linear`,
+                location: `California`,
+                email: `jfasulkey@gmail.com`,
+                url: `https://jeffs.geocities.page`,
+                github: `https://github.com/fazulk`,
+                name: `Jeff Fasulkey`,
+                objective: `To apply my dynamic skill-set in a workplace that thrives on providing the highest quality products and service while also generating an excellent user experience.`,
+                languages: [
+                    `Javascript, ES6+, Node.js, MongoDB, Oracle, SQL, Yarn, NPM`,
+                    `Google Cloud Project, Cloud Functions, SSR, Docker, Kubernetes`,
+                    `Effeciant, scalable application architecture & design patterns`,
+                    `Vue.js, HTML5,\tCSS, SASS, Webpack, Babel, JSON, Application Security`,
+                    `Excel, Photoshop, Illustrator, Dreamweaver, Flash`,
+                    `OSX, Windows 7, 10`
+                ]
+            })
     }
 }
