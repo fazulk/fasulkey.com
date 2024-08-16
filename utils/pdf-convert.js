168d0dec2ab915d52fbd7fccfe45c447650f0fbe
Jeff Fasulkey <fazulk@gmail.com>
2024-08-15 21:06:24 -0700

    do this work

--- utils/pdf-convert.js
+++ utils/pdf-convert.js
@@ -1,8 +1,15 @@
 import fs from 'node:fs'
-import puppeteer from 'puppeteer'
+// import puppeteer from 'puppeteer'
+import chromium from '@sparticuz/chromium'
+import puppeteer from 'puppeteer-core'
 
 async function convertHtmlToPdf(htmlFilePath, outputPdfPath) {
-  const browser = await puppeteer.launch()
+//   const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  })
   const page = await browser.newPage()
 
   // Load the HTML file
